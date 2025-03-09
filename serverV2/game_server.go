fea1653ab8ab31c14b481ce8563247523cd0f1b5
Harley <harley@summitdigitalgp.com>
2025-03-08 21:53:22 -0700

    commit

---
+++ serverV2/game_server.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+var gameNumber = 0 
+var startPort = 8080
+
+
+
+type Game struct {
+	Id 	int	
+	Players []Player	
+} 
+
+type Player struct {
+	Number int
+}
+
+type IpAddress string
+type PortNumber int 
+type GameCode string
+
+type GamePlayerPair struct {
+	game *Game
+	player *Player
+}
+
+type Lobby map[GameCode][]IpAddress
+
+var GameConnections = make(map[IpAddress]GamePlayerPair)   
+
+var GameList = make(map[PortNumber]*Game)
+
+// func broadcastGameState() {
+	
+// }
+
+func initGame()*Game {
+	game := &Game{}
+	game.Players = make([]Player, 0)
+	return game
+}
+
+func (g *Game)createPlayer() *Player{
+	player := Player{len(g.Players)}
+	g.Players = append(g.Players, player)
+	return &player
+}
+
+func handlePlayerConnection(portNumber PortNumber) http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request) {
+		ws, err := upgrader.Upgrade(w, r, nil)
+
+		if err != nil {
+			log.Fatalf("Failed to upgrade to websocket: %v", err)
+		}
+
+		ip := IpAddress(ws.RemoteAddr().String())
+
+		defer ws.Close()
+		var game *Game
+		var player *Player
+
+		value, exists := GameConnections[ip]
+
+		if !exists {
+			game = GameList[portNumber]		
+			player = game.createPlayer()	
+			GameConnections[ip] = GamePlayerPair{game, player}
+		}else{
+			// game = value.game
+			player = value.player
+		}
+
+		playerNumber := map[string]int{"playerNumber": player.Number}	
+
+		ws.WriteJSON(playerNumber)
+	}
+}
+
+func startGame(portNumber PortNumber) {
+	game := initGame()
+	GameList[portNumber] = game
+
+	slashPort := fmt.Sprintf("/%v", portNumber)
+	http.HandleFunc(slashPort, handlePlayerConnection(portNumber))
+}
+
+func buildHeader(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Content-Type", "application/json")
+}
+
+func handlePlay(w http.ResponseWriter, r *http.Request) {
+	buildHeader(&w)
+	gameNumber++
+	portNumber := PortNumber(startPort+gameNumber)
+
+	log.Printf("Starting game on port %v", portNumber)
+	res := make(map[string]PortNumber)
+	res["data"] = portNumber
+
+	json.NewEncoder(w).Encode(res)
+
+	startGame(portNumber)
+}
+
+func handleJoin(w http.ResponseWriter, r *http.Request){
+	ws, err := upgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		log.Fatalf("Failed to upgrade to websocket: %v", err)
+	}
+
+	gameCode := r.URL.Query()
+	log.Printf("%v", gameCode)
+
+	ws.WriteMessage(0,[]byte("hello"))
+}
+
+func main() {
+	http.HandleFunc("/play", handlePlay)
+	http.HandleFunc("/join", handleJoin)
+
+	port:= fmt.Sprintf(":%v", startPort)
+	log.Fatal(http.ListenAndServe(port, nil))
+}
