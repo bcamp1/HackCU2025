c8b304cb663b83a1bc5fd2cd297c6e4dbfdcc159
Branson Camp <bransoncamp@outlook.com>
2025-03-08 23:54:18 -0700

    client goodness

---
+++ clientV2/dist/lobby/lobby.js
@@ -0,0 +1,43 @@
+console.log("LOBBY")
+
+const startButton = document.getElementById("start-game-button")
+const gameIdLabel = document.getElementById("game-id-label")
+const playerList = document.getElementById("player-list")
+
+const host = "10.0.0.43"
+
+// Get params
+const urlSearchParams = new URLSearchParams(window.location.search)
+const gameCode = urlSearchParams.get('gameCode').toUpperCase();
+const playerName = urlSearchParams.get('name');
+gameIdLabel.innerText = `Code: ${gameCode}`
+
+console.log(gameIdLabel)
+console.log(startButton)
+console.log(gameCode)
+console.log(playerName)
+
+// Render Player List
+playerNames = ['Steven', 'Jeff', 'Jordan', 'Mike']
+
+// Websocket
+const ws = new WebSocket(`ws://${host}:8080/join?gameCode=${gameCode}&name=${playerName}`)
+
+ws.addEventListener('message', (event) => {
+	data = JSON.parse(event.data)
+	console.log(data)
+	playerNames = data.names ?? []
+	console.log(playerNames)
+
+	playerList.children = []
+
+	playerNames.forEach(element => {
+		playerText = document.createElement("p");
+		playerText.innerText = element
+		playerList.appendChild(playerText)
+	})
+})
+
+startButton?.addEventListener("click", async () => {
+	// TODO
+})
