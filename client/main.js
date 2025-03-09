130795cc19d44df0170c84629e3b226bd939672c
Harley <harley@summitdigitalgp.com>
2025-03-08 23:17:07 -0700

    backend lobbies

--- client/main.js
+++ client/main.js
@@ -3,5 +3,4 @@ const startButton = document.getElementById("start-game-button")
 startButton.addEventListener("click", async () => {
 	const res = await fetch("http://localhost:8080/start")
 	const j = await res.json()
-	window.location.assign("http://localhost:3000/play?port=" + j.data)
 })
